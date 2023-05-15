cd6f20075f0d028fe2a7980c9d4774e0abcbf4d1
Mina-Eissa <minaeissa268@gmail.com>
2023-05-15 10:01:04 +0300

    add TimeSpent column in sql files 6,10,11,12 and update student_progress to return name and timespent

--- project/Back-End/student_progress.py
+++ project/Back-End/student_progress.py
@@ -19,8 +19,6 @@ router = APIRouter()
 
 def get_data(myresult, mycursor):
     cols = [i[0] for i in mycursor.description]
-    print(len(myresult[0]))
-    print(len(cols))
     row_dict = {}
     for i in range(len(cols)):
         if myresult[0][i] is not None:
@@ -36,16 +34,21 @@ async def get_students_enrolled_in_book(SID=None, BID=None, CHID=None, SECID=Non
     if SID is None and BID is None and CHID is None and SECID is None:
         return data
 
-    query_general = "SELECT SID,BID,SUM(NUMBER_OF_ATTEMPTS) AS BOOK_NUM_OF_ATTEMPTS,SUM(NUMBER_OF_HINTS) AS BOOK_NUM_OF_HINTS,SUM(NUMBER_OF_INCORRECT_ATTEMPTS) AS BOOK_NUM_OF_INCORRECT_ATTEMPTS FROM STUDENT_BOOK WHERE SID=%s AND BID=%s"
-    query_chapter = "SELECT SID,CHID,SUM(NUMBER_OF_ATTEMPTS) AS CHAPTER_NUM_OF_ATTEMPTS,SUM(NUMBER_OF_HINTS) AS CHAPTER_NUM_OF_HINTS,SUM(NUMBER_OF_INCORRECT_ATTEMPTS) AS CHAPTER_NUM_OF_INCORRECT_ATTEMPTS FROM STUDENT_CHAPTER WHERE SID=%s AND CHID=%s"
-    query_section = "SELECT SID,SECID,SUM(NUMBER_OF_ATTEMPTS) AS SECTION_NUM_OF_ATTEMPTS,SUM(NUMBER_OF_HINTS) AS SECTION_NUM_OF_HINTS,SUM(NUMBER_OF_INCORRECT_ATTEMPTS) AS SECTION_NUM_OF_INCORRECT_ATTEMPTS FROM STUDENT_SECTION WHERE SID=%s AND SECID=%s"
+    query_general = "SELECT SID,BID,SUM(NUMBER_OF_ATTEMPTS) AS BOOK_NUM_OF_ATTEMPTS,SUM(NUMBER_OF_HINTS) AS BOOK_NUM_OF_HINTS,SUM(NUMBER_OF_INCORRECT_ATTEMPTS) AS BOOK_NUM_OF_INCORRECT_ATTEMPTS,TIMESPENT FROM STUDENT_BOOK WHERE SID=%s AND BID=%s"
+    query_chapter = "SELECT SID,CHID,SUM(NUMBER_OF_ATTEMPTS) AS CHAPTER_NUM_OF_ATTEMPTS,SUM(NUMBER_OF_HINTS) AS CHAPTER_NUM_OF_HINTS,SUM(NUMBER_OF_INCORRECT_ATTEMPTS) AS CHAPTER_NUM_OF_INCORRECT_ATTEMPTS,TIMESPENT FROM STUDENT_CHAPTER WHERE SID=%s AND CHID=%s"
+    query_section = "SELECT SID,SECID,SUM(NUMBER_OF_ATTEMPTS) AS SECTION_NUM_OF_ATTEMPTS,SUM(NUMBER_OF_HINTS) AS SECTION_NUM_OF_HINTS,SUM(NUMBER_OF_INCORRECT_ATTEMPTS) AS SECTION_NUM_OF_INCORRECT_ATTEMPTS,TIMESPENT FROM STUDENT_SECTION WHERE SID=%s AND SECID=%s"
 
+    mycursor.execute("Select concat(sfirst_name,' ',slast_name) as name from student\
+                     where SID = %s ", (SID,))
+    student_name = mycursor.fetchall()[0][0]
+    
     mycursor.execute(query_general, (SID, BID,))
     myresult = mycursor.fetchall()
     if myresult is not None:
         row = get_data(myresult, mycursor)
         row["SID"] = int(SID)
         row["BID"] = int(BID)
+        row["Name"] = student_name
         data.append(row)
 
     if CHID:
@@ -55,16 +58,18 @@ async def get_students_enrolled_in_book(SID=None, BID=None, CHID=None, SECID=Non
             row = get_data(myresult, mycursor)
             row["SID"] = int(SID)
             row["CHID"] = int(CHID)
+            row["Name"] = student_name
             data.append(row)
 
     if SECID:
         mycursor.execute(query_section, (SID, SECID,))
         myresult = mycursor.fetchall()
         if myresult is not None:
+            row = get_data(myresult, mycursor)
             row["SID"] = int(SID)
             row["SECID"] = int(SECID)
-            row = get_data(myresult, mycursor)
+            row["Name"] = student_name
             data.append(row)
-
+    
     json_data = jsonable_encoder(data)
     return json_data
